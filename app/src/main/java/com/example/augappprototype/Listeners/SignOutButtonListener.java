e78fa3eb302d168a41e7783708d6d9f376578d7b
Peter Zeng <shichun1@ualberta.ca>
2018-01-19 01:06:03 -0700

    There's now a confirmation prompt for signing out.

--- app/src/main/java/com/example/augappprototype/Listeners/SignOutButtonListener.java
+++ app/src/main/java/com/example/augappprototype/Listeners/SignOutButtonListener.java
@@ -1,5 +1,7 @@
 package com.example.augappprototype.Listeners;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.ImageButton;
@@ -31,7 +33,25 @@ public class SignOutButtonListener implements View.OnClickListener {
     /*--Methods--*/
     @Override
     public void onClick(View view) {
-        Intent goToLogin = new Intent(mainMenu, LoginScreen.class);
-        mainMenu.startActivity(goToLogin);
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainMenu);
+        builder.setCancelable(true);
+        builder.setTitle("Signing out");
+        builder.setMessage("Are you sure you want to sign out?");
+        builder.setPositiveButton("Confirm",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent goToLogin = new Intent(mainMenu, LoginScreen.class);
+                        mainMenu.startActivity(goToLogin);
+                    }
+                });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }//onClick
 }//SignOutButtonListener
--- app/src/main/java/com/example/augappprototype/Listeners/SignOutMainActivityListener.java
+++ app/src/main/java/com/example/augappprototype/Listeners/SignOutMainActivityListener.java
@@ -1,5 +1,7 @@
 package com.example.augappprototype.Listeners;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
@@ -26,18 +28,27 @@ public class SignOutMainActivityListener implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        signoutButtonListener();
-    } // onClick(View)
 
-    public void signoutButtonListener(){
-        Button signout = mainActivity.findViewById(R.id.signOutButton);
-        signout.setOnClickListener(new View.OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+        builder.setCancelable(true);
+        builder.setTitle("Signing out");
+        builder.setMessage("Are you sure you want to sign out?");
+        builder.setPositiveButton("Confirm",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent goToLogin = new Intent(mainActivity, LoginScreen.class);
+                        mainActivity.startActivity(goToLogin);
+                    }
+                });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent goToLogin = new Intent(mainActivity, LoginScreen.class);
-                mainActivity.startActivity(goToLogin);
+            public void onClick(DialogInterface dialog, int which) {
             }
         });
-    } // signoutButtonListener()
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    } // onClick(View)
 
 } // SignOutMainActivityListener
