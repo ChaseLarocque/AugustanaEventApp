2d68e084aa026cf0aa2f1f5a105b05f3e5c17718
Peter Zeng <shichun1@ualberta.ca>
2018-01-19 22:54:35 -0700

    Got rid of OtherMainMenuButtonListener.java

--- app/src/main/java/com/example/augappprototype/MainMenu.java
+++ app/src/main/java/com/example/augappprototype/MainMenu.java
@@ -1,25 +1,15 @@
 package com.example.augappprototype;
 import android.content.Intent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-
-import android.widget.Toast;
 
 import android.widget.Toast;
 
 import com.example.augappprototype.Listeners.AugustanaLibraryListener;
 import com.example.augappprototype.Listeners.AugustanaNewsletterListener;
 import com.example.augappprototype.Listeners.AugustanaWebsiteListener;
-import com.example.augappprototype.Listeners.EventCalendarListener;
-import com.example.augappprototype.Listeners.OtherMainMenuButtonListeners;
 import com.example.augappprototype.Listeners.SignOutButtonListener;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -91,11 +81,11 @@ public class MainMenu extends AppCompatActivity {
         });
 
         findViewById(R.id.newsletterImage)
-                .setOnClickListener(new OtherMainMenuButtonListeners(this));
+                .setOnClickListener(new AugustanaNewsletterListener(this));
         findViewById(R.id.libraryImage)
-                .setOnClickListener(new OtherMainMenuButtonListeners(this));
+                .setOnClickListener(new AugustanaLibraryListener(this));
         findViewById(R.id.websiteImage)
-                .setOnClickListener(new OtherMainMenuButtonListeners(this));
+                .setOnClickListener(new AugustanaWebsiteListener(this));
         findViewById(R.id.signout)
                 .setOnClickListener(new SignOutButtonListener(this));
     }//registerMenuButtons
