e70562c83150d2a633f1809024f099e1d583c93d
ChaseLarocque <32780762+ChaseLarocque@users.noreply.github.com>
2018-01-19 20:32:12 -0700

    cleaned code

--- app/src/main/java/com/example/augappprototype/MainActivity.java
+++ app/src/main/java/com/example/augappprototype/MainActivity.java
@@ -20,6 +20,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
